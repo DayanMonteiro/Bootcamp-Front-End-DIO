15d27a1d81fde85a1e7304a9931dacbbf9cac6c6
Dayan Monteiro <dmsmamb@gmail.com>
2020-09-09 16:55:45 -0300

    js

---
+++ Introdução ao Javascript/js/main.js
@@ -0,0 +1,15 @@
+
+var nome = "Dayan Monteiro";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLocaleUpperCase());
+/*                 toLocaleUpperCase deixa tudo em maiúscula / para minuscula toLowerCase*/
+
+//alert(frase.replace("Japão" , "Brasil"));
+
+
